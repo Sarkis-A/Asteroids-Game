0671192984323a8adb13713ebf764fc5f79eaad5
Sarkis-A <austin.sarkis@outlook.com>
2025-05-18 12:40:05 -0600

    Updated

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    update_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (update_group, drawable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+
     
     while True:
         for event in pygame.event.get():
@@ -16,8 +21,11 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        update_group.update(dt)
+        
+        for sprite in drawable_group:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
