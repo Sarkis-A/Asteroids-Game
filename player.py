c8d651c086686f373a007ce32d7cc752f6b09f86
Sarkis-A <austin.sarkis@outlook.com>
2025-05-17 20:10:37 -0600

    Updated

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 import pygame
-from circleshape import Circleshape
+from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 
-class Player(Circleshape):
+class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
